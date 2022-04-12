477f7cdee774329fa7a27845b3f9ea2d2e6a9853
toddjudd <todd.judd@gmail.com>
2022-04-12 11:24:43 -0600

    feat(init): Initializeing repo
    Using CRA-Template-Toddjudd
    Added Steele Fonts and colors

--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -3,18 +3,26 @@ import { Logo } from '@/components/Elements';
 export const Landing = () => {
   return (
     <div className='text-center'>
-      <header className='bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-white text-xl'>
+      <header className='bg-cream min-h-screen flex flex-col items-center justify-center text-white text-xl'>
         <Logo />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. changes
+        <p className='text-steele'>
+          Edit <code className='text-deep-sea-blue'>src/App.tsx</code> and save
+          to reload. changes
         </p>
         <a
-          className='text-[#61dafb]'
+          className='text-earth'
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
           Learn React
         </a>
+        <p className='text-sage font-montserrat'>
+          Montserat - Steele Family Moments
+        </p>
+        <p className='text-sage font-code'>Code - Steele Family Moments</p>
+        <p className='text-sage font-brittany'>
+          Brittany - Steele Family Moments
+        </p>
       </header>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      cream: '#f5f3ef',
+      sage: '#8e9f83',
+      earth: '#9f9485',
+      'deep-sea-blue': '#658589',
+      steele: '#95999A',
+    },
     extend: {
+      fontFamily: {
+        brittany: ['Brittany', 'cursive'],
+        code: ['Code', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
       animation: {
         'spin-slow': 'spin 20s linear infinite',
       },
